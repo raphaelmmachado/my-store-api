fc490598eecec7b07901853609966e242b32bf9e
Raphael Machado <raphael.mm.91@gmail.com>
2022-07-18 12:17:23 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = mongoose.Schema(
   {
-    name: { type: String, requied: true, minLength: 3, maxLength: 50 },
+    name: { type: String, required: true, minLength: 3, maxLength: 50 },
     email: {
       type: String,
       required: true,
